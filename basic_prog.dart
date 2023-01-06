8c4ff0a5ebec08def542f2ae9c72376d5393c930
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-07 00:25:30 +0300

    variables

--- basic_prog.dart
+++ basic_prog.dart
@@ -1,4 +1,10 @@
 // My first dart program
 void main() {
   print("Hello world");
+  // Variables
+  var name = "Denzel";
+  var phone = "0714082283";
+  var email = "gitongadenzel@gmail.com";
+  print(
+      "Hello $name,\n your phone number is $phone\n and your email is $email");
 }
