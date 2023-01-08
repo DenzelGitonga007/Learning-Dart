394d52809f60b7d45ff924dce4dc902684d5faa8
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-08 19:34:36 +0300

    my app

--- first_app/bin/first_app.dart
+++ first_app/bin/first_app.dart
@@ -1,5 +1,6 @@
 import 'package:first_app/first_app.dart' as first_app;
 
 void main(List<String> arguments) {
-  print('Hello world: ${first_app.calculate()}!');
+  print(
+      'Hello world, this is Denzel\'s first app in Dart: ${first_app.calculate()}!');
 }
