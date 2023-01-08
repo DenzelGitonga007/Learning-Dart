2d55cbb69818497cc84b25d3cd1bac8378151ce1
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-08 18:02:38 +0300

    Dart calculations

--- basic_prog.dart
+++ basic_prog.dart
@@ -7,4 +7,10 @@ void main() {
   var email = "gitongadenzel@gmail.com";
   print(
       "Hello $name,\n your phone number is $phone\n and your email is $email");
+  // Calculations
+  int num1 = 12;
+  int num2 = 15;
+  int sum = num1 + num2;
+  double div = num2 / num1; //Return a decimal
+  print("The sum is $sum, and the quotient is $div");
 }
