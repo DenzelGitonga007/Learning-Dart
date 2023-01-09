764610da028a2133385aef81af0404c17bdf0066
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-09 13:31:14 +0300

    formatting strings

--- advanced_datatypes.dart
+++ advanced_datatypes.dart
@@ -1,5 +1,11 @@
+// Trying out the format method as in Python
+// import 'dart:io';
+// import 'package:format/format.dart';
+
 void main(List<String> args) {
   List<String> name = ["Denzel", "Catherine", "Gitonga"];
   print(name);
   print(name.length);
+  // print("My list has $name\.length names: {}".format(name.length, name));
+  print("My name list has ${name.length} names: $name");
 }
