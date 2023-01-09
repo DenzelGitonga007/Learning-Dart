f449ae462aa12607c4fc62d58b4dbf7e8eec13ff
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-09 13:52:37 +0300

    sets-- no duplicate values

--- advanced_datatypes.dart
+++ advanced_datatypes.dart
@@ -1,6 +1,7 @@
 // Trying out the format method as in Python
 // import 'dart:io';
 // import 'package:format/format.dart';
+import 'trying_sets.dart';
 
 void main(List<String> args) {
   List<String> name = ["Denzel", "Catherine", "Gitonga"];
@@ -8,4 +9,6 @@ void main(List<String> args) {
   print(name.length);
   // print("My list has $name\.length names: {}".format(name.length, name));
   print("My name list has ${name.length} names: $name");
+  Set<String> myDays = {"Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"};
+  print("Days of the week are: $myDays");
 }
---
+++ trying_sets.dart
@@ -0,0 +1,4 @@
+void main(List<String> args) {
+  Set<String> week = {"Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"};
+  print(week);
+}
