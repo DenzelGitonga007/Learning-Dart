8973cc5af609b2c3d2e3575626e3c571a157bf03
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-09 13:57:37 +0300

    maps

--- advanced_datatypes.dart
+++ advanced_datatypes.dart
@@ -1,14 +1,22 @@
 // Trying out the format method as in Python
 // import 'dart:io';
 // import 'package:format/format.dart';
-import 'trying_sets.dart';
 
 void main(List<String> args) {
+  // List
   List<String> name = ["Denzel", "Catherine", "Gitonga"];
   print(name);
   print(name.length);
   // print("My list has $name\.length names: {}".format(name.length, name));
   print("My name list has ${name.length} names: $name");
+  // Sets
   Set<String> myDays = {"Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"};
   print("Days of the week are: $myDays");
+  // Maps
+  Map<String, String> myDetails = {
+    'Name': 'Denzel Gitonga',
+    'Phone': '0714082283',
+    'Email': 'gitongadenzel@gmail.com'
+  };
+  print("My details are: $myDetails");
 }
