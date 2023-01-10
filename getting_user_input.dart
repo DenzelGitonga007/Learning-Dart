3e76ac76e1b31d26f5173c4679efbeeba3c14586
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-10 11:31:04 +0300

    int input

--- getting_user_input.dart
+++ getting_user_input.dart
@@ -4,5 +4,7 @@ void main(List<String> args) {
   // # Get user input
   print("Enter your name: ");
   String? name = stdin.readLineSync();
-  print("You are: $name");
+  print("How old are you? (In numbers only)");
+  int? age = int.parse(stdin.readLineSync()!);
+  print("Hi $name, you are $age years old");
 }
