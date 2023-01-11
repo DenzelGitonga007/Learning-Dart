5058e24e8369146c07a686f0a39916e811e04d55
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-11 11:29:06 +0300

    question 1-- print name

--- practice/solutions/answers.dart
+++ practice/solutions/answers.dart
@@ -1,1 +1,9 @@
+// 1. Write a program to print your name in Dart.
+import 'dart:io';
 
+void main(List<String> args) {
+  // Get the user input for name
+  print("What is your name?😊: ");
+  String? name = stdin.readLineSync();
+  print("Hi $name 😊");
+}
