58e08c354877e75cfa766f7e694c1007f447a54c
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-15 20:47:24 +0300

    print only even numbers

--- conditions_and_loops/loops/for_loop.dart
+++ conditions_and_loops/loops/for_loop.dart
@@ -7,4 +7,20 @@ void main(List<String> args) {
   for (int i = 10; i >= 1; i--) {
     print(i);
   }
+  // Add all numbers in the loop
+  var total = 0;
+  print("Add the values in a loop");
+  for (int i = 1; i <= 10; i++) {
+    total += 1;
+  }
+  print(
+      total); // to get the last value of total, the print func should be outside the loop
+
+  // Print only even numbers
+  print("Even numbers from 50 to 100:");
+  for (int i = 50; i <= 100; i++) {
+    if (i % 2 == 0) {
+      print(i);
+    }
+  }
 }
