6f24290c2fdcc5191e6e03f27a7b6f7dd59bf7ac
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-15 20:39:24 +0300

    for loop in descening order

--- conditions_and_loops/loops/for_loop.dart
+++ conditions_and_loops/loops/for_loop.dart
@@ -3,4 +3,8 @@ void main(List<String> args) {
   for (int i = 1; i <= 10; i++) {
     print(i.toString() + " Hi Denzel");
   }
+  // Print in descending order
+  for (int i = 10; i >= 1; i--) {
+    print(i);
+  }
 }
